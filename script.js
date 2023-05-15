a3d2b1a1caeb47a601514e36bdee337c05dee50b
JL <yxutxpia@gmail.com>
2023-05-15 18:08:07 +0900

    .

--- script.js
+++ script.js
@@ -3,8 +3,7 @@
 const iconsax = document.querySelectorAll('.iconsax');
 
 const generateIconsax = async function (icon, type, label) {
-  const url = `https://raw.githubusercontent.com/thejayl/iconsax/main/src/iconsax/${type}/${label}.svg`;
-
+  const url = `https://raw.githubusercontent.com/thejayl/iconsax/main/iconsax/${type}/${label}.svg`;
   fetch(url)
     .then((res) => {
       return res.text();
@@ -14,8 +13,17 @@ const generateIconsax = async function (icon, type, label) {
     });
 };
 
+const iconStyler = (icon) => {
+  icon.style.width = 'fit-content';
+  icon.style.height = 'fit-content';
+  icon.style.display = 'inline-flex';
+  icon.style.justifyContent = 'center';
+  icon.style.alignItems = 'center';
+};
+
 for (const icon of iconsax) {
   const iconType = icon.getAttribute('type');
   const iconLabel = icon.getAttribute('icon');
   generateIconsax(icon, iconType, iconLabel);
+  iconStyler(icon);
 }
