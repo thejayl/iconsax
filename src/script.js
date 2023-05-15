8ae2be5d8918c0f2e05fc6cf09a11ca9e5688e49
JL <yxutxpia@gmail.com>
2023-05-15 17:15:18 +0900

    Update script code

--- src/script.js
+++ src/script.js
@@ -2,23 +2,20 @@
 
 const iconsax = document.querySelectorAll('.iconsax');
 
-const getScript = async function (type, icon) {
-  const parser = new DOMParser();
-  const url =
-    'https://raw.githubusercontent.com/thejayl/iconsax/main/src/img/icons.svg';
-  const icons = await fetch(`${url}#${icon}`);
-  const data = icons.text();
-  const html = parser.parseFromString(data, 'text/html');
-  const svg = html.querySelector('body');
-  console.log(svg);
+const generateIconsax = async function (icon, type, label) {
+  const url = `https://raw.githubusercontent.com/thejayl/iconsax/main/src/iconsax/${type}/${label}.svg`;
+
+  fetch(url)
+    .then((res) => {
+      return res.text();
+    })
+    .then((data) => {
+      icon.innerHTML = data;
+    });
 };
 
 for (const icon of iconsax) {
   const iconType = icon.getAttribute('type');
   const iconLabel = icon.getAttribute('icon');
-  getScript(iconType, iconLabel);
+  generateIconsax(icon, iconType, iconLabel);
 }
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   getScript();
-// });
